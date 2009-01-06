f88d8fe23295e38f7270006a894ced21ea97073f
Bernd Bischl <bischl@statistik.tu-dortmund.de>
2009-01-06 15:09:47 +0000

    commons: changes to imagamanager, remove com.sun.*

--- src/af/commons/io/ImageManager.java
+++ src/af/commons/io/ImageManager.java
@@ -1,8 +1,8 @@
 package af.commons.io;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Image;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.ConvolveOp;
 import java.awt.image.Kernel;
@@ -10,30 +10,23 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import javax.swing.ImageIcon;
-
-import com.sun.image.codec.jpeg.JPEGCodec;
-import com.sun.image.codec.jpeg.JPEGEncodeParam;
-import com.sun.image.codec.jpeg.JPEGImageEncoder;
-
-public class ImageManager {	
+public class ImageManager {
     
-	public static void soften(BufferedImage bufferedImage) {
+	public static BufferedImage soften(BufferedImage bufferedImage) {
 		float[] softenArray = {-1, -1, -1,
 				-1,  9, -1,
 				-1, -1, -1};
 		Kernel kernel = new Kernel(3, 3, softenArray);
 		ConvolveOp cOp = new ConvolveOp(kernel, ConvolveOp.EDGE_NO_OP, null);
-		bufferedImage = cOp.filter(bufferedImage, null);
-		bufferedImage = cOp.filter(bufferedImage, null);
+		return cOp.filter(bufferedImage, null);
 	}
 	
     public static void resize(File originalFile, File resizedFile, int newWidth, float quality, boolean soften)
         throws IOException {
 
-        if (quality < 0 || quality > 1) {
-            throw new IllegalArgumentException("Quality has to be between 0 and 1");
-        }
+//        if (quality < 0 || quality > 1) {
+//            throw new IllegalArgumentException("Quality has to be between 0 and 1");
+//        }
 
         ImageIcon ii = new ImageIcon(originalFile.getCanonicalPath());
         Image i = ii.getImage();
@@ -63,20 +56,14 @@ public class ImageManager {
         g.drawImage(temp, 0, 0, null);
         g.dispose();
 
-        if (soften) { soften(bufferedImage); }
+        if (soften) {
+            bufferedImage = soften(bufferedImage); 
+        }
         
         // Write the jpeg to a file.
         FileOutputStream out = new FileOutputStream(resizedFile);
 
-        // Encodes image as a JPEG data stream
-        JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
-
-        JPEGEncodeParam param = encoder.getDefaultJPEGEncodeParam(bufferedImage);
-
-        param.setQuality(quality, true);
-
-        encoder.setJPEGEncodeParam(param);
-        encoder.encode(bufferedImage);
+        ImageIO.write(bufferedImage, "jpg", resizedFile);
     }
 
 }
