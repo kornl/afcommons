8551fc68b0c552ff0c21d4ceb0d83c83dfb99a6b
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2009-02-22 21:33:45 +0000

    afcommons pdf

--- src/af/commons/io/pdf/PDFHelper.java
+++ src/af/commons/io/pdf/PDFHelper.java
@@ -1,15 +1,31 @@
 package af.commons.io.pdf;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.rmi.RemoteException;
+import java.util.List;
+import java.util.Vector;
 
 import com.lowagie.text.BadElementException;
+import com.lowagie.text.Cell;
+import com.lowagie.text.Chunk;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Font;
 import com.lowagie.text.Image;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Rectangle;
 import com.lowagie.text.pdf.PdfImportedPage;
+import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfReader;
 import com.lowagie.text.pdf.PdfWriter;
 
 public class PDFHelper {
+	
+    public static Font font = new Font(Font.TIMES_ROMAN, 10, Font.NORMAL);
+	public static Font fontb = new Font(Font.TIMES_ROMAN, 10, Font.BOLD);
+	public static Font fonth = new Font(Font.TIMES_ROMAN, 12, Font.BOLD);
 	
 	/**
 	 * Returns a com.lowagie.text.Image from a image or PDF file.
@@ -61,4 +77,55 @@ public class PDFHelper {
 		}
 		return result;
 	}
+	
+	/**
+	 * Writes a Result-List and its list of files in a PDF document.
+	 * @param document PDF document
+	 * @param writer PdfWriter
+	 * @param result Result-List
+	 * @param plots List of plot files
+	 * @throws IOException 
+	 * @throws DocumentException
+	 */
+	public static PdfPTable createTable(List<List<Object>> xtable, List<String> header) throws BadElementException, IOException {		
+		PdfPTable table = new PdfPTable(xtable.get(0).size());
+		Cell cell;
+		
+		if (header!=null) {
+			for (String s : header) {
+				cell = new Cell(new Chunk(s, fontb));
+				cell.enableBorderSide(Rectangle.LEFT);
+				cell.enableBorderSide(Rectangle.RIGHT);
+				cell.enableBorderSide(Rectangle.BOTTOM);
+				cell.setBorderWidth(0.5f);
+				table.addCell(cell.createPdfPCell());
+			}
+		}
+
+		for (List<Object> row : xtable) {			
+			for (Object e : row) {
+				if (e instanceof BufferedImage) {
+					cell = new Cell(Image.getInstance((BufferedImage)e, null));
+				} else {				
+					cell = new Cell(new Chunk(e.toString(), font));
+				}
+				enable4Borders(cell);
+				table.addCell(cell.createPdfPCell());
+			}
+		}
+		return table;
+	}
+	
+	/**
+	 * Sets the four borders of a cell to 0.5f.
+	 * @param cell Cell which borders are set.
+	 */
+	public static void enable4Borders(Cell cell) {
+		cell.enableBorderSide(Rectangle.LEFT);
+		cell.enableBorderSide(Rectangle.RIGHT);
+		cell.enableBorderSide(Rectangle.TOP);
+		cell.enableBorderSide(Rectangle.BOTTOM);
+		cell.setBorderWidth(0.5f);
+	}
+	
 }
