7e84c9f8243a34915927c6acad20046bd05e11a9
Bernd Bischl <bischl@statistik.tu-dortmund.de>
2009-08-18 16:36:18 +0000

    commons: test for windows shortcut

---
+++ src/tests/DesktopShortcutWindowsTest.java
@@ -0,0 +1,23 @@
+package tests;
+
+import org.af.commons.install.WindowsDesktopShortcut;
+
+import java.io.File;
+import java.io.IOException;
+
+public class DesktopShortcutWindowsTest {
+    public static void main(String[] args) {
+        try {
+            File d = new File(System.getProperty("user.home"), "Desktop");
+            WindowsDesktopShortcut sc = new WindowsDesktopShortcut(d, "Test1");
+            sc.setExec(new File("C:\\Programme\\Adobe\\Reader 8.0\\Reader\\acrord32.exe"));
+            sc.createDesktopEntry();
+            sc = new WindowsDesktopShortcut(d, "Test2");
+            sc.setExec(new File("C:\\Programme\\Adobe\\Reader 8.0\\Reader\\acrord32.exe"));
+            sc.addParameter(new File("c:\\test.pdf"));
+            sc.createDesktopEntry();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
