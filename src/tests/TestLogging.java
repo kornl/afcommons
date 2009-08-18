0a77e05483a7b48d4828ccb5988374506c7665f4
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2009-08-18 12:30:17 +0000

    Irrelevant.

--- src/tests/TestLogging.java
+++ src/tests/TestLogging.java
@@ -2,17 +2,16 @@ package tests;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.log4j.Logger;
 
 public class TestLogging {
     protected static final Log logger = LogFactory.getLog(TestLogging.class);
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         Logger.getRootLogger().getAllAppenders();
         if (logger.isInfoEnabled()) {
             System.out.println("info is enabled");
             logger.info("bla");
         }
-    }
+    }*/
 }
 
