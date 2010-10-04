05c8252296b95a76b67127779bbee68b6d670c12
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2010-10-04 08:56:17 +0000

    Advanced error report.

--- src/org/af/commons/errorhandling/report.php
+++ src/org/af/commons/errorhandling/report.php
@@ -4,16 +4,27 @@ include_once('Mail.php');
 include_once('Mail/mime.php');
 
 $text = "Dies ist der Bugreport\n";
+$subject = 'Bugreport';
+
+$text = $text . "USER AGENT: " . $_SERVER['HTTP_USER_AGENT']. "\n";
+$text = $text . "REFERER   : " . $_SERVER['HTTP_REFERER']. "\n";
+$text = $text . "REMOTE IP : " . $_SERVER['REMOTE_ADDR']. "\n";
+$text = $text . "HOST NAME : " .gethostbyaddr ($_SERVER['REMOTE_ADDR']). "\n";
 
 foreach ($_POST as $key => $value) {
   $text = $text . $key . ": " . $value . "\n";
   echo $key . ": " . $value . "<br>";
+  if ($key == "Subject") {
+  	$subject = $value;
+  	$subject = str_replace ("@", "(at)", $subject);
+  	$subject = str_replace ("\\", "/", $subject);  
+  }
 }
 
 $crlf = "\n";
 $hdrs = array(
               'From'    => 'bugreport@small-projects.de',
-              'Subject' => 'Bugreport'
+              'Subject' => $subject
               );
 
 $mime = new Mail_mime($crlf);
