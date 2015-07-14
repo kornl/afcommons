0a1119c6574d2109ed088cbd8fa6de6c09118e13
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2015-07-15 01:05:28 +0200

    Added reading XML from String and transforming XML to String objects.

--- src/org/af/commons/io/XMLIO.java
+++ src/org/af/commons/io/XMLIO.java
@@ -4,10 +4,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.io.StringWriter;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -35,9 +37,23 @@ public class XMLIO {
 	}
 	
 	/**
-	 * Speichert eine XML-Datei.
+	 * Reads a XML file.
+	 * @param string xml file that should be read in.
+	 * @throws ParserConfigurationException 
+	 * @throws IOException 
+	 * @throws SAXException 
+	 */
+	public static Document readXML(String string) throws ParserConfigurationException, SAXException, IOException {
+		Document document = null;
+		DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		document = builder.parse(string);
+	    return document;
+	}
+	
+	/**
+	 * Saves a XML file.
 	 * @param document the xml document to be saved.
-	 * @param filename the file the xml document should be saved to.
+	 * @param file the file the xml document should be saved to.
 	 * @throws TransformerFactoryConfigurationError 
 	 * @throws FileNotFoundException 
 	 * @throws TransformerException 
@@ -48,4 +64,24 @@ public class XMLIO {
 		StreamResult result = new StreamResult(new PrintStream(file));
 		transformer.transform(source, result);
 	}
+	
+	/**
+	 * Transforms a xml document to a String
+	 * @param document the xml document to be saved.
+	 * @param file the file the xml document should be saved to.
+	 * @throws TransformerFactoryConfigurationError 
+	 * @throws FileNotFoundException 
+	 * @throws TransformerException 
+	 */
+	public static String xml2String(Document document, File file) throws TransformerFactoryConfigurationError, FileNotFoundException, TransformerException {
+		Transformer transformer = TransformerFactory.newInstance().newTransformer();
+		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+		DOMSource source = new DOMSource(document);
+		StringWriter stringWriter = new StringWriter();
+		StreamResult result = new StreamResult(stringWriter);
+		transformer.transform(source, result);
+		return stringWriter.getBuffer().toString(); //.replaceAll("\n|\r", "");
+	}
+	
+
 }
